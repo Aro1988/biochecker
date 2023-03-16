32e061531848e648296d9b809ad76751b0131856
Marc Burchart <marc.burchart@fernuni-hagen.de>
2023-03-16 22:05:15 +0100

    initial commit

---
+++ core.py
@@ -0,0 +1,101 @@
+import cv2
+import os
+import numpy as np
+import math
+import csv
+
+def generateMask(image, minBrightness):  
+    grayedImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    threshold = cv2.threshold(grayedImage, minBrightness, 255, cv2.THRESH_BINARY)[1]
+    kernel = np.ones((2,2), np.uint8)
+    morphOpen = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel)
+    return morphOpen
+
+def maskExtract(image, mask):
+    extractedImages = []
+    invertedMask = cv2.bitwise_not(mask)
+    extractedImages.append(cv2.bitwise_and(image, image, mask=mask))
+    extractedImages.append(cv2.bitwise_and(image, image, mask=invertedMask))
+    return extractedImages
+
+def calculateMeanBrightness(image):
+    height, width, channels = image.shape
+    pixelCount = 0
+    brightnessSum = 0
+    for y in range(height):
+        for x in range(width):
+            pixel = image[y,x]
+            if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+                continue
+            brightness = math.sqrt(0.299 * math.pow(pixel[0], 2) + 0.587 * math.pow(pixel[1], 2) + 0.114 * math.pow(pixel[2], 2))
+            brightnessSum += brightness
+            pixelCount += 1
+    divison = 0
+    if pixelCount > 0:
+        divison = brightnessSum/pixelCount
+    return [divison, brightnessSum, pixelCount]
+
+def removeBrightPixel(image, minBrightness):
+    height, width, channels = image.shape
+    for y in range(height):
+        for x in range(width):
+            pixel = image[y,x]
+            if pixel[0] == 0 and pixel[1] == 0 and pixel[2] == 0:
+                continue
+            brightness = math.sqrt(0.299 * math.pow(pixel[0], 2) + 0.587 * math.pow(pixel[1], 2) + 0.114 * math.pow(pixel[2], 2))
+            if brightness >= minBrightness:
+                image[y,x] = np.array([0,0,0])
+    return image
+
+def switchDarkPixel(foregroundImage, backgroundImage, maxBrightness):  
+    height, width, channels = foregroundImage.shape
+    for y in range(height):
+        for x in range(width):
+            pixel = foregroundImage[y,x]
+            brightness = math.sqrt(0.299 * math.pow(pixel[0], 2) + 0.587 * math.pow(pixel[1], 2) + 0.114 * math.pow(pixel[2], 2))
+            if brightness <= maxBrightness:
+                backgroundImage[y, x] = pixel
+                foregroundImage[y, x] = [0, 0, 0]
+
+def loadImage(imagePath):
+    if not os.path.exists:
+        raise Exception(f"Bild {imagePath} existiert nicht.")
+    image = cv2.imread(imagePath)
+    return image
+
+def sanitizeAndCheckPath(path):
+    path = os.path.normpath(path)
+    if not os.path.exists(path):
+        raise Exception(f"Der Pfad {path} wurde nicht gefunden.")
+    return path
+
+def sanitizeAndCheckFactor(factor):
+    try:
+        factor = float(factor)
+        return factor
+    except ValueError:
+        raise Exception(f"Das Argument {factor} ist keine Kommazahl.")
+
+def findCh01Files(path):
+    files = []
+    fileList = os.listdir(path)
+    for fileName in fileList:
+        if 'ch01' not in fileName:
+            continue
+        else:
+            files.append(fileName)
+    return files
+
+def getFileBasename(fileName):
+    name = fileName.split('_')
+    del name[-1]
+    name = '_'.join(name)
+    return name
+
+def getMatchingCh02FileName(ch01File):
+    baseName = getFileBasename(ch01File)
+    ch02name = baseName + '_ch02.tif'
+    return ch02name
+
+def saveImage(image, filePath):
+    cv2.imwrite(filePath, image)
---
+++ run.py
@@ -0,0 +1,65 @@
+import core
+import sys
+import os
+import csv
+
+
+try:
+    args = sys.argv
+    if len(args) != 6:
+        raise Exception('Ungültige Paramter (Quellpfad, Zielpfad, UntererFaktorRot, UntererFaktorGrün, ObererFaktorGrün)')
+    sourcePath = core.sanitizeAndCheckPath(args[1])
+    targetPath = core.sanitizeAndCheckPath(args[2])
+    lowerCh02Factor = core.sanitizeAndCheckFactor(args[3])
+    lowerCh01Factor = core.sanitizeAndCheckFactor(args[4])
+    upperCh01Factor = core.sanitizeAndCheckFactor(args[5])     
+    findCh01Files = core.findCh01Files(sourcePath)
+    csvPath = os.path.join(targetPath, 'results.csv')   
+    if os.path.exists(csvPath):
+        os.remove(csvPath)
+    csvFile = open(csvPath, 'w', newline='')
+    writer = csv.writer(csvFile)
+    writer.writerow(['sample', 'transfected is brighter', 'brightning_transfected', 'pixelcount_transfected', 'sumbrightness_transfected', 'brightning_untransfacted', 'pixelcount_untransfacted', 'sumbrightness_untransfacted'])
+    for ch01FileName in findCh01Files:
+        baseName = core.getFileBasename(ch01FileName)
+        print(f"Verarbeite {baseName}...")
+        ch02FileName = core.getMatchingCh02FileName(ch01FileName)
+        ch01FilePath = os.path.join(sourcePath, ch01FileName)
+        ch02FilePath = os.path.join(sourcePath, ch02FileName)
+        ch01image = core.loadImage(ch01FilePath)
+        ch02image = core.loadImage(ch02FilePath)
+        ch02MeanBrightness = core.calculateMeanBrightness(ch02image)
+        if lowerCh02Factor <= 0:
+            minThreshold = 0
+        else:
+            minThreshold = lowerCh02Factor * ch02MeanBrightness[0]
+        print(f"Unterer Threshold für das ch02 Bild: {minThreshold}")
+        mask = core.generateMask(ch02image, minThreshold)
+        maskFilePath = os.path.join(targetPath, f"{baseName}_mask.tif")
+        core.saveImage(mask, maskFilePath)
+        ch01MeanBrightness = core.calculateMeanBrightness(ch01image)
+        ch01Foreground, ch01Background = core.maskExtract(ch01image, mask)
+        core.saveImage(ch01Foreground, os.path.join(targetPath, f"{baseName}_transfected.tif"))
+        core.saveImage(ch01Background, os.path.join(targetPath, f"{baseName}_untransfected.tif"))
+        print(f"Unterer Threshold für das ch01 Bild: {ch01MeanBrightness[0] * lowerCh01Factor}")
+        if lowerCh01Factor > 0:
+            core.switchDarkPixel(ch01Foreground, ch01Background, ch01MeanBrightness[0] * lowerCh01Factor)
+        core.saveImage(ch01Background, os.path.join(targetPath, f"{baseName}_untransfected_cleaned.tif"))
+        if upperCh01Factor > 0:
+            ch01Foreground = core.removeBrightPixel(ch01Foreground, ch01MeanBrightness[0] * upperCh01Factor)
+            print(f"Oberer Threshold für das ch01 Bild: {ch01MeanBrightness[0] * lowerCh01Factor}")
+        else:
+            print(f"Oberer Threshold für das ch01 Bild: 0")
+        core.saveImage(ch01Foreground, os.path.join(targetPath, f"{baseName}_transfected_cleaned.tif"))
+        foregroundMeanBrightness = core.calculateMeanBrightness(ch01Foreground)
+        backgroundMeanBrightness = core.calculateMeanBrightness(ch01Background)
+        res = 0
+        if foregroundMeanBrightness[0] > backgroundMeanBrightness[0]:
+            res = 1
+        writer.writerow([baseName, res, foregroundMeanBrightness[0], foregroundMeanBrightness[1], foregroundMeanBrightness[2], backgroundMeanBrightness[0], backgroundMeanBrightness[1], backgroundMeanBrightness[2]])
+    print('Ausführung beendet.')
+except Exception as ex:
+    print('Ein Fehler ist aufgetreten:')
+    print(ex)
+    import traceback
+    print(traceback.format_exc())
